de7422636250ebaa4c273c160fddc67c8f9237e1
Arly Fernandez <arly.miguel.fernandez@gmail.com>
2019-02-11 10:47:09 -0500

    js updated

--- bin/main/static/js/custom.js
+++ bin/main/static/js/custom.js
@@ -2,11 +2,45 @@
  * product controller
  */
 
-function show(){
+function Producto(){
+	this.productList = $('#productList');
+	this.dropProduct = $('#dropProduct');
+	this.showbtn = $('#showbtn');
+}
+
+Producto.prototype.init = function () {
+	this.handler();
+}
+
+Producto.prototype.handler = function () {
+	var obj = this;
 	
-	$('#productList > option:selected').each(function() {
-		console.log($(this).text() + ' ' + $(this).val());
+	this.dropProduct.click(function () {
+		obj.deleteProduct();
+    });
+	
+	this.showbtn.click(function () {
+		obj.deleteProduct();
 	});
 	
-	//https://productservices.herokuapp.com/api/armifella/product-managment/v1/product
 }
+
+Producto.prototype.deleteProduct = function () {
+	$("#productList option:selected").remove();
+}
+
+Producto.prototype.undo = function () {
+	
+}
+
+Producto.prototype.show = function () {
+	this.productList("> option:selected").each(function() {
+		console.log($(this).text() + ' ' + $(this).val());
+		});
+	
+//	$('#productList > option:selected').each(function() {
+//		console.log($(this).text() + ' ' + $(this).val());
+//	});
+}
+
+//https://www.w3schools.com/howto/howto_css_modals.asp
